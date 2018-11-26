01607d3e41ebcb0f29fae43f6566494e75a1c512
Angrid <my_dawn@ukr.net>
2018-11-27 00:29:49 +0200

    nav update

--- dist/templates/forty/js/index.js
+++ dist/templates/forty/js/index.js
@@ -3,8 +3,11 @@ window.addEventListener('load', () => {
   let menuShow = document.getElementById('button-menu');
   let menuHide = document.getElementById('button-menu-close');
   let navOverlay = document.querySelector('.nav-overlay');
+  let headerNav = document.querySelector('.header-main');
 
-  menuShow.addEventListener('click', (e) => {    
+  menuShow.addEventListener('click', (e) => {
+    headerNav.classList.remove('show');
+    headerNav.classList.add('hide');
     navOverlay.classList.remove('hide');
     navOverlay.classList.add('show');
   });
@@ -20,5 +23,7 @@ window.addEventListener('load', () => {
   function hideMenu() {
     navOverlay.classList.remove('show');
     navOverlay.classList.add('hide');
+    headerNav.classList.remove('hide');
+    headerNav.classList.add('show');
   }
 });
