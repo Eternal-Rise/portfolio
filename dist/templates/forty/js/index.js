5584b6f8ac357995c00ac5057f4a51c2b897bee8
Angrid <my_dawn@ukr.net>
2018-11-27 09:43:52 +0200

    add blur under nav-overlay

--- dist/templates/forty/js/index.js
+++ dist/templates/forty/js/index.js
@@ -4,12 +4,15 @@ window.addEventListener('load', () => {
   let menuHide = document.getElementById('button-menu-close');
   let navOverlay = document.querySelector('.nav-overlay');
   let headerNav = document.querySelector('.header-main');
+  let blur = document.getElementById('blur');
 
   menuShow.addEventListener('click', (e) => {
     headerNav.classList.remove('show');
     headerNav.classList.add('hide');
     navOverlay.classList.remove('hide');
     navOverlay.classList.add('show');
+    blur.classList.remove('off')
+    blur.classList.add('on')
   });
 
   menuHide.addEventListener('click', hideMenu);
@@ -25,5 +28,7 @@ window.addEventListener('load', () => {
     navOverlay.classList.add('hide');
     headerNav.classList.remove('hide');
     headerNav.classList.add('show');
+    blur.classList.remove('on')
+    blur.classList.add('off')
   }
 });
