735cbc97d7d43dd0de13fbb0e4f17bf099f8dc90
Angrid <my_dawn@ukr.net>
2018-11-28 16:24:44 +0200

    initial end

--- dist/templates/forty/js/index.js
+++ dist/templates/forty/js/index.js
@@ -5,7 +5,7 @@ window.addEventListener('load', () => {
   let menuHide = document.getElementById('button-menu-close');
   let navOverlay = document.querySelector('.nav-overlay');
   let headerNav = document.querySelector('.header-bar');
-  let blur = document.getElementById('blur');
+  let blur = document.getElementById('wrapper');
 
   menuShow.addEventListener('click', (e) => {
     headerNav.classList.remove('show');
@@ -33,30 +33,55 @@ window.addEventListener('load', () => {
     blur.classList.add('off')
   }
 
-
-  // Parallax under banner
-  let banner = document.querySelector('.banner');
-
-  window.addEventListener('scroll', () => {
-    let scroll = window.pageYOffset;
-    banner.style.backgroundPosition = `center ${scroll / 8}px`
-  })
-
-  // Scroll to tiles
-  let btnScroll = document.getElementById('scroll');
-
-  btnScroll.addEventListener('click', () => {
+  if (window.location.pathname.search(/index|landing.html/) != -1) {
     
-    let target = document.querySelector('.tiles');
-    let offset = target.getBoundingClientRect().top;
-    let headerNavHeight = parseInt(window
+    // Parallax under banner
+    let banner = document.querySelector('.banner');
+
+    window.addEventListener('scroll', () => {
+      let scroll = window.pageYOffset;
+      banner.style.backgroundPosition = `center -${scroll / 4}px`
+
+      headerScroll()
+    })
+
+    // Scroll to end of banner
+    let btnScroll = document.getElementById('scroll');
+    
+    const headerNavHeight = parseInt(window
       .getComputedStyle(headerNav,null)
       .getPropertyValue("height"));
 
-    window.scroll({
-      top: offset - headerNavHeight,
-      left: 0,
-      behavior: 'smooth'
-    });
+    const offset = parseInt(window
+      .getComputedStyle(banner,null)
+      .getPropertyValue("height"));
+
+    function headerScroll() {
+      if (window.pageYOffset > headerNavHeight) {
+        headerNav.style.transform = 'translateY(-100%)';
+        headerNav.style.webkitTransform = 'translateY(-100%)';
+        
+        if (window.pageYOffset >= (offset - headerNavHeight)) {
+          headerNav.classList.add('fixed')
+          headerNav.style.transform = 'translateY(0)';
+          headerNav.style.webkitTransform = 'translateY(0)';
+        }
+      } else if (window.pageYOffset <= headerNavHeight) {
+        headerNav.classList.remove('fixed')
+        headerNav.style.transform = 'translateY(0)';
+        headerNav.style.webkitTransform = 'translateY(0)';
+        }
+      }
+      
+      if (window.location.pathname.search(/index.html/) != -1) {
+        btnScroll.addEventListener('click', () => {
+          window.scroll({
+            top: offset - headerNavHeight,
+            left: 0,
+            behavior: 'smooth'
+          });
+        });
+      }
+    }
   });
-});
+  
