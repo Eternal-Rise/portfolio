cad17c64ab766c1b3b992d50eb133b38a7f2987b
Angrid <my_dawn@ukr.net>
2018-12-14 19:54:20 +0200

    fix slide

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,26 +1,17 @@
 window.addEventListener('load', () => {
 
-  window.addEventListener('scroll', () => {
-    console.log(window.screen.height);
-  })
-
-  console.log(document.body.clientHeight, window.screen.height)
-
   const scroll = document.getElementsByClassName('scroll');
-  let multiplier = 1;
   
   for (let i = 0; i < scroll.length; i++) {
-    console.log(window.screen.height * multiplier)
 
     scroll[i].addEventListener('click', () => {
       window.scroll({
-        top: window.screen.height * multiplier,
+        top: window.screen.height * (i + 1),
         left: 0, 
         behavior: 'smooth' 
       });
     });
 
-    multiplier++
   }
 
 })
