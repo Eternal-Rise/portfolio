a8c413ab2c83651f365f131704cd4277e74e1d60
Angrid <my_dawn@ukr.net>
2018-12-15 07:53:58 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 window.addEventListener('load', () => {
 
   const scroll = document.getElementsByClassName('scroll');
+  const slide = document.querySelector('.slide');
+  console.log(window.screen.height)
   
   for (let i = 0; i < scroll.length; i++) {
 
     scroll[i].addEventListener('click', () => {
       window.scroll({
-        top: window.screen.height * (i + 1),
+        top: slide.clientHeight * (i + 1),
         left: 0, 
         behavior: 'smooth' 
       });
