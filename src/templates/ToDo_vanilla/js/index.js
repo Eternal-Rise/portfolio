5c19163e4679c520d9d4c6888de8560664bcc7f2
Eternal-Rise <my_dawn@ukr.net>
2019-08-13 23:19:07 +0300

    base idea

--- src/templates/ToDo_vanilla/js/index.js
+++ src/templates/ToDo_vanilla/js/index.js
@@ -1,1 +1,35 @@
 console.log("i'm running");
+
+const $ = (elem, local = undefined) => local ? local.querySelector(elem) : document.querySelector(elem);
+const $All = (elem, local = undefined) => local ? local.querySelectorAll(elem) : document.querySelectorAll(elem);
+
+const input = $('#note');
+const output = $('#notes');
+const tNote = $('p',  $('#t-note').content );
+const submit = $('#submit');
+
+const notes = localStorage.getItem('notes') || '[]';
+const notesArr = JSON.parse(notes);
+
+const createNote = text => {
+  const note = tNote.cloneNode();
+  note.textContent = text;
+  return note;
+};
+
+for ( const note of notesArr ) {
+  output.appendChild( createNote( note ) );
+}
+
+submit.addEventListener('click', e => {
+  e.preventDefault();
+
+  if (input.value) {
+    const note = tNote.cloneNode();
+    note.textContent = input.value;
+    output.appendChild( note );
+
+    notesArr.push(input.value);
+    localStorage.setItem('notes', JSON.stringify(notesArr))
+  }
+});
