d434a3311e4c66cab7c0c12532d3019277ea143e
Eternal-Rise <my_dawn@ukr.net>
2019-08-15 22:44:54 +0300

    list localStorage

--- src/templates/ToDo_vanilla/js/index.js
+++ src/templates/ToDo_vanilla/js/index.js
@@ -10,9 +10,34 @@ const $All = (elem, parent = undefined) => parent ? parent.querySelectorAll(elem
 // 2. 
 
 const findLevel = item => item.classList.value.match(/_level-[1-9]/)[0];
+const data = JSON.parse(localStorage.getItem('data')) || {};
+const output = $('.output');
 
 const list = $('.list');
 const item = $('.list__item', list);
+
+const createItem = (list, e) => {
+  
+  if (!e.shiftKey && e.keyCode === 13) {
+    e.preventDefault();
+
+    const level = findLevel(e.target);
+    const newItem = document.createElement('li');
+    
+    newItem.addEventListener('keydown', createItem.bind(null, list));
+    newItem.addEventListener('keydown', deleteItem);
+    newItem.addEventListener('keydown', shiftItem);
+    newItem.addEventListener('paste', pasteText);
+
+    newItem.classList.add('list__item', level);
+
+    // idk. wanna string
+    newItem.contentEditable = 'true';
+    
+    list.insertBefore(newItem, e.target.nextElementSibling);
+    newItem.focus();
+  }
+}
 
 const deleteItem = e => {
   if (e.keyCode === 8 || e.keyCode === 46) {
@@ -47,31 +72,88 @@ const shiftItem = e => {
   }
 }
 
-const createItem = e => {
-  
-  if (e.keyCode === 13) {
-    e.preventDefault();
-
-    const level = findLevel(e.target);
-    const newItem = document.createElement('li');
-    
-    newItem.addEventListener('keydown', createItem);
-    newItem.addEventListener('keydown', deleteItem);
-    newItem.addEventListener('keydown', shiftItem);
-
-    newItem.classList.add('list__item', level);
-
-    // idk. wanna string
-    newItem.contentEditable = 'true';
-    
-    list.insertBefore(newItem, e.target.nextElementSibling);
-    newItem.focus();
-  }
+const pasteText = e => {
+  // console.log(e.keyCode);
+  let paste = (e.clipboardData || window.clipboardData).getData('text');
+  e.preventDefault();
+  e.target.ownerDocument.execCommand("insertText", false, paste);
 }
 
-item.addEventListener('keydown', createItem);
+item.addEventListener('keydown', createItem.bind(null, list));
 item.addEventListener('keydown', deleteItem);
 item.addEventListener('keydown', shiftItem);
+item.addEventListener('paste', pasteText);
+
+const form = $('.form');
+const formFileds = $('.form__fields');
+const submit = $('.form__submit');
+
+submit.addEventListener('click', e => {
+  e.preventDefault();
+
+  const list = $('.list');
+  // cut list to output
+  output.insertBefore(list, output.firstElementChild);
+
+  // create new list & item
+  const newList = document.createElement('ol');
+  const newItem = document.createElement('li');
+
+  newList.classList.add('list');
+  newList.appendChild(newItem);
+  formFileds.appendChild(newList);
+
+  newItem.addEventListener('keydown', createItem.bind(null, newList));
+  newItem.addEventListener('keydown', deleteItem);
+  newItem.addEventListener('keydown', shiftItem);
+  newItem.addEventListener('paste', pasteText);
+
+  newItem.classList.add('list__item', '_level-1');
+
+  // idk. wanna string
+  newItem.contentEditable = 'true';
+
+  newItem.focus()
+
+
+  // save to localStorage
+  const items = [...$All('.list__item', list)];
+
+  if (!Array.isArray(data.list)) { data.list = [] };
+
+  for (const item of items) {
+    data.list.push({
+      text: item.textContent,
+      classList: item.classList.value.split(' '),
+    });
+  }
+  localStorage.setItem('data', JSON.stringify(data));
+});
+
+
+if (Object.keys(data)) {
+
+  const list = document.createElement('ol');
+  for ( const dataItem of data.list ) {
+
+    const item = document.createElement('li');
+
+    list.classList.add('list');
+    list.appendChild(item);
+    
+    item.addEventListener('keydown', createItem.bind(null, list));
+    item.addEventListener('keydown', deleteItem);
+    item.addEventListener('keydown', shiftItem);
+    item.addEventListener('paste', pasteText);
+    
+    item.classList.add(...dataItem.classList);
+    item.textContent = dataItem.text;
+
+    // idk. wanna string
+    item.contentEditable = 'true';
+  }
+  output.appendChild(list);
+}
 
 
 
