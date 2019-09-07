aa5461c5c436dc204e901d26feb03f8792d5d372
Eternal-Rise <my_dawn@ukr.net>
2019-09-07 09:17:04 +0300

    add auto changing slides

--- src/templates/Key_Development/blocks/slider/slider.js
+++ src/templates/Key_Development/blocks/slider/slider.js
@@ -1,19 +1,18 @@
 const slides = document.querySelectorAll('.slider__item');
 const changeSlideBtn = document.querySelector('.slider-nav__btn');
-const progress = document.querySelector('.slider-nav__progress');
-const progressValue = progress.querySelector('.progress__value');
+const timeline = document.querySelector('.slider-nav__progress');
+const timelineValue = timeline.querySelector('.progress__value');
 
-const step = 100 / (slides.length - 1);
+// const step = 100 / (slides.length - 1);
 
 let isForwardDirection = true;
 let i = 0;
 
-changeSlideBtn.addEventListener('click', () => {
-
+const changeSlide = () => {
   if (isForwardDirection) {
     slides[i].classList.remove('_active');
     slides[i + 1].classList.add('_active');
-    progressValue.style.width = `${step * (i + 1)}%`;
+    // progressValue.style.width = `${step * (i + 1)}%`;
 
     i++;
     if (i === slides.length - 1) {
@@ -23,11 +22,46 @@ changeSlideBtn.addEventListener('click', () => {
   } else {
     slides[i].classList.remove('_active');
     slides[i - 1].classList.add('_active');
-    progressValue.style.width = `${step * (i - 1)}%`;
+    // progressValue.style.width = `${step * (i - 1)}%`;
     i--;
     if (i === 0) {
       isForwardDirection = true;
       changeSlideBtn.classList.remove('_prev');
     }
   }
+};
+
+window.addEventListener('load', () => {
+
+  // 5s + 50sm for reset _loading;
+  const SLIDE_DURATION = 5050;
+  const INTRO_DURATION = 3250;
+  let autoChangeSlide;
+
+  const resetTimeLineValue = () => {
+    timelineValue.classList.remove('_loading');
+    setTimeout(() => timelineValue.classList.add('_loading'), 50);
+  };
+
+  setTimeout(() => {
+    autoChangeSlide = setTimeout(function fn() {
+      resetTimeLineValue();
+      changeSlide();
+      autoChangeSlide = setTimeout(fn, SLIDE_DURATION);
+    }, SLIDE_DURATION);
+
+    timelineValue.classList.add('_loading');
+  }, INTRO_DURATION);
+
+  changeSlideBtn.addEventListener('click', changeSlide);
+  changeSlideBtn.addEventListener('click', () => {
+    resetTimeLineValue();
+    clearInterval(autoChangeSlide);
+
+    autoChangeSlide = setTimeout(function fn() {
+      resetTimeLineValue();
+      changeSlide();
+      autoChangeSlide = setTimeout(fn, SLIDE_DURATION);
+    }, SLIDE_DURATION);
+  });
 });
